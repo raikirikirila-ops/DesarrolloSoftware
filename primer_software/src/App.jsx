2eebf0990c3a0c6a45b42439c4646ea3355d1db5
CavalliB <119020580+CavalliB@users.noreply.github.com>
2025-08-30 01:20:07 +0000

    ciclo 5 xd

--- primer_software/src/App.jsx
+++ primer_software/src/App.jsx
@@ -3,12 +3,13 @@ import './App.css'
 import Principal from './principal.jsx'
 import Buscador from './buscador.jsx'
 import Playlist from './playlist.jsx'
+import Usuario from './usuario.jsx'
 
 function App() {
   const [seccion, setSeccion] = useState('principal')
 
   return (
-    <div> 
+    <div>
       <nav>
         <ul>
           <li>
@@ -20,12 +21,17 @@ function App() {
           <li>
             <button onClick={() => setSeccion('playlist')}>Playlist</button>
           </li>
+          <li>
+            <button className='usericon' onClick={() => setSeccion('usuario')}><img className='usericon' src="https://imgs.search.brave.com/uRLjIz0r9LwrGq9jagcfeSqoD188L_55nkk0IhaFSrw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvOTk5/NDQ1MzIyL3Bob3Rv/L25hbmR1LXJoZWEt/YW1lcmljYW5hLWdy/ZWF0ZXItcmhlYS5q/cGc_cz02MTJ4NjEy/Jnc9MCZrPTIwJmM9/NGc2MDRpUFBPbDBH/LXlzalBKalVnTmdx/bnFvMGl4SFFGWGhK/dnRhMWRFWT0" alt="Icono" /> </button>
+          </li>
         </ul>
       </nav>
       <main>
         {seccion === 'principal' && <Principal />}
         {seccion === 'buscador' && <Buscador />}
         {seccion === 'playlist' && <Playlist />}
+        {seccion === 'usuario' && <Usuario />}
+
       </main>
       <footer>Reproductor</footer>
     </div>
---
+++ primer_software/src/usuario.jsx
@@ -0,0 +1,12 @@
+function usuario() {
+    return (
+        <div className="fullicon" >
+            <h2>juan</h2>
+            <h2>perez</h2>  
+            <h2>Plan: pro max</h2>
+            <h2>fecha de vencimiento: mañana</h2>
+            <button>guardar</button>
+        </div>
+    )
+}
+export default usuario;
