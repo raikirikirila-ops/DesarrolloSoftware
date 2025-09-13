8c9e402e0478075c030a1d3ca3adb831736ea6f4
CavalliB <119020580+CavalliB@users.noreply.github.com>
2025-09-13 01:24:10 +0000

    subasta v1

---
+++ Subasta/src/principal.jsx
@@ -0,0 +1,18 @@
+function Principal() {
+    return (
+        <>
+        <div className='containerpreviwbox'>
+            <div className='previewbox'>holaa </div>
+            <div className='previewbox'>holaa </div>
+            <div className='previewbox'>holaa </div>
+            <div className='previewbox'>holaa </div>
+            <div className='previewbox'>holaa </div>
+            <div className='previewbox'>holaa </div>
+            <div className='previewbox'>holaa </div>
+            <div className='previewbox'>holaa </div>
+            <div className='previewbox'>holaa </div>
+</div>
+        </>
+    )
+}
+export default Principal;
