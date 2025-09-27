ad779b03613856be694168f5d353814ecc05ac97
CavalliB <119020580+CavalliB@users.noreply.github.com>
2025-09-27 03:29:28 +0000

    pagina principal

--- 1v1code/src/App.jsx
+++ 1v1code/src/App.jsx
@@ -1,10 +1,25 @@
+import { useState } from 'react';
 import CodeEditor from './CodeEditor';
+import './App.css';
 
 function App() {
+  const [section, setSection] = useState("home");
+
   return (
-    <div>
-      <CodeEditor />
-    </div>
+    <>
+      <section className='boxApp' style={{ display: section === "home" ? "block" : "none" }}>
+        <h1>Bienvenido a 1v1Code!</h1>
+        <button className="botoncenter" onClick={() => setSection("code")}>jugar</button>
+        <button className="botoncenter" onClick={() => setSection("user")}>Ver perfil</button>
+      </section>
+      <section style={{ display: section === "code" ? "block" : "none" }}>
+        <CodeEditor />
+      </section>    
+      <section style={{ display: section === "user" ? "block" : "none" }}>
+        
+      </section>    
+      </>
   );
 }
+
 export default App;
